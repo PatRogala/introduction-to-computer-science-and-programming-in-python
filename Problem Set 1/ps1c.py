479578df26769a143e1aeea4ec249723130c7d57
Patryk Rogala <rogala.patryk@pm.me>
2022-10-29 17:19:47 +0200

    Pset1 Fix

--- Problem Set 1/ps1c.py
+++ Problem Set 1/ps1c.py
@@ -12,20 +12,20 @@ steps = 0
 while abs(current_savings - total_cost * portion_down_payment) > 100:
     current_savings = 0
     steps += 1
-    portion_saved = (low + high) / 2
+    portion_saved = (low + high) // 2
     monthly_salary = annual_salary / 12
     for month in range(1, 37):
         current_savings += current_savings * r / 12
         current_savings += monthly_salary * portion_saved / 10000
         if month % 6 == 0:
             monthly_salary += monthly_salary * semi_annual_raise
+    if (low + high) // 2 == low:
+        print("It is not possible to pay the down payment in three years.")
+        exit()
     if current_savings < total_cost * portion_down_payment:
         low = portion_saved
     else:
         high = portion_saved
 
-if steps > 13:
-    print("It is not possible to pay the down payment in three years.")
-else:
-    print("Best savings rate:", portion_saved / 10000)
-    print("Steps in bisection search:", steps)
+print("Best savings rate:", portion_saved / 10000)
+print("Steps in bisection search:", steps)
