94c35aa3fea6de1b328b82fffc830ac751d9b7b2
Patryk Rogala <rogala.patryk@pm.me>
2022-11-02 14:08:22 +0100

    Problem 5

--- Problem Set 5/ps5.py
+++ Problem Set 5/ps5.py
@@ -120,10 +120,11 @@ class DescriptionTrigger(PhraseTrigger):
 # TIME TRIGGERS
 
 # Problem 5
-# TODO: TimeTrigger
-# Constructor:
-#        Input: Time has to be in EST and in the format of "%d %b %Y %H:%M:%S".
-#        Convert time from string to a datetime before saving it as an attribute.
+class TimeTrigger(Trigger):
+    def __init__(self, time):
+        self.time = datetime.strptime(time, "%d %b %Y %H:%M:%S")
+
+    def get_time(self): return self.time
 
 # Problem 6
 # TODO: BeforeTrigger and AfterTrigger
